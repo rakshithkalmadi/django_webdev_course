b06fa4556daf5f5bbb9ce6b4e40c58e92d5bc72e
rakshithkalmadi <58518378+rakshithkalmadi@users.noreply.github.com>
2021-10-27 22:24:21 +0530

    Add files via upload

---
+++ django_course/javascript/excersice1.js
@@ -0,0 +1,5 @@
+alert("Hello");
+var lbs = prompt("Enter your weight in lbs: ");
+kg=lbs*0.454;
+alert("Your weight in kg is: "+kg);
+console.log("Complete");
---
+++ django_course/javascript/one.js
@@ -0,0 +1,4 @@
+alert("Welcome to this page");
+var deposit=prompt("How much would you like to deposite");
+alert("You have deposited: "+deposit);
+console.log("You are a cool person!");
